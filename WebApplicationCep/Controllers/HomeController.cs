8ab980fa714fb554867c54a2922963e3ab1bc062
Thiago Borges Vieira <thiagoborges@amhp.com.br>
2018-02-16 11:42:23 -0200

    Projeto de exemplo

---
+++ WebApplicationCep/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationCep.Models;
+
+namespace WebApplicationCep.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Models.Cep cep)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cep);
+            }
+
+            var correios = new Correios.AtendeClienteClient();
+
+            var consulta = correios.consultaCEPAsync(cep.Codigo.Replace("-", "")).Result;
+
+            if (consulta != null)
+            {
+                ViewBag.Endereco = new Models.Endereco()
+                {
+                    Descricao = consulta.@return.end,
+                    Complemento = consulta.@return.complemento,
+                    Bairro = consulta.@return.bairro,
+                    Cidade = consulta.@return.cidade,
+                    UF = consulta.@return.uf
+                };
+            }
+
+            return View(cep);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplicationCep/Models/Cep.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationCep.Models
+{
+    public class Cep
+    {
+        [Required]
+        [MaxLength(9, ErrorMessage = "Cep tem que ser digitado no formato 70000-000")]
+        [MinLength(9, ErrorMessage = "Cep tem que ser digitado no formato 70000-000")]
+        public string Codigo { get; set; }
+    }
+}
